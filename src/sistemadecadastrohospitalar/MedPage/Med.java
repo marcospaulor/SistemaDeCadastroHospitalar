e74c17769eb56abd4fc0a14684dd446c51861b59
gabrielPistore <gabriel.oliveira.pistore@gmail.com>
2021-04-22 10:36:11 -0300

    pequena alteração em Med.java e TableData.java

--- src/sistemadecadastrohospitalar/MedPage/Med.java
+++ src/sistemadecadastrohospitalar/MedPage/Med.java
@@ -457,39 +457,6 @@ public class Med extends javax.swing.JFrame {
             System.err.println("Erro: " + e);
         }
     }
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Med.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Med.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Med.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Med.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> {
-            new Med().setVisible(true);
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextArea diagnosticoTA;
@@ -517,6 +484,7 @@ public class Med extends javax.swing.JFrame {
     private javax.swing.JLabel tipoSanguineLB;
     private javax.swing.JButton voltarBtn;
     // End of variables declaration//GEN-END:variables
+    
     private boolean isAnyEmptyFields() {
         return pressaoFTF.getText().isEmpty() || temperaturaFTF.getText().isEmpty() || 
                 sintomasTA.getText().isEmpty() ||
